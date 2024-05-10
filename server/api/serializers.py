9cbcc29bc368676c0886e94449f0796c7387997a
g1Den1s-it <g1den1splay@gmail.com>
2024-05-10 23:02:53 +0300

    added Auction serializer

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import MetadataNFT
+from .models import MetadataNFT, Auction
 
 
 class MetadataNFTSerialzier(serializers.ModelSerializer):
     class Meta:
         model = MetadataNFT
         fields = ['name', 'description', 'image']
+
+
+class AuctionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Auction
+        fields = ["name", "description", "image",
+                  "goal", "collected", "min_price",
+                  "owner", "date"]
