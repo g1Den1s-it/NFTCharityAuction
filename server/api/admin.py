5e717ebe9c1ae6f921fff3e34d0cbde77c3a779a
g1Den1s-it <g1den1splay@gmail.com>
2024-05-12 23:55:33 +0300

    connected models to admin panel

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from.models import MetadataNFT
+from.models import MetadataNFT, Auction, RewardList
 # Register your models here.
 
 admin.site.register(MetadataNFT)
+admin.site.register(Auction)
+admin.site.register(RewardList)
