16341e102f308e6334e7c4be3d91d13f0beff1f9
g1Den1s-it <g1den1splay@gmail.com>
2024-05-12 23:38:38 +0300

    added auction list endpoint

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListAPIViewMetaDataNFT
+from .views import ListAPIViewMetaDataNFT, ListAPIViewAuction
 
 urlpatterns = [
     path('v1/', ListAPIViewMetaDataNFT.as_view(), name="metadata"),
+    path('v1/aution/', ListAPIViewAuction.as_view(), name="list-auction"),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
-from .models import MetadataNFT
-from .serializers import MetadataNFTSerialzier
+from .models import MetadataNFT, Auction
+from .serializers import MetadataNFTSerialzier, AuctionSerializer
+
+
 # Create your views here.
 class ListAPIViewMetaDataNFT(ListAPIView):
+    """view get metadate of current nft-token"""
     serializer_class = MetadataNFTSerialzier
 
     def get(self, request, *args, **kwargs):
         queryset = get_object_or_404(MetadataNFT, pk=1)
         serializer = MetadataNFTSerialzier(queryset).data
         return Response(serializer)
+
+
+class ListAPIViewAuction(ListAPIView):
+    """get list of all data of open auction from database"""
+    queryset = Auction.objects.filter(is_open=True)
+    serializer_class = AuctionSerializer
+
