4e0a584051e4fc66da84870a299ad2aace3b374e
g1Den1s-it <g1den1splay@gmail.com>
2024-05-18 02:17:25 +0300

    added statistic and endpoint

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import ListAPIViewMetaDataNFT, ListAPIViewAuction, RetrieveAPIViewAuction
+from .views import (
+        ListAPIViewMetaDataNFT,
+        ListAPIViewAuction,
+        RetrieveAPIViewAuction,
+        StatisticDataApiView,
+    )
 
 urlpatterns = [
     path('v1/', ListAPIViewMetaDataNFT.as_view(), name="metadata"),
     path('v1/auction/', ListAPIViewAuction.as_view(), name="list-auction"),
     path('v1/auction/<int:id>/', RetrieveAPIViewAuction.as_view(), name="detail-auction"),
+    path('v1/statistic/', StatisticDataApiView.as_view(), name="statistic"),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, GenericAPIView
+from web3 import Web3
+from eth_user.models import User
 from .models import MetadataNFT, Auction
 from .serializers import MetadataNFTSerialzier, AuctionSerializer
 
@@ -36,3 +40,28 @@ class RetrieveAPIViewAuction(RetrieveAPIView):
             return Response(serializer)
 
         return Response({})
+
+
+class StatisticDataApiView(GenericAPIView):
+    """general statistic of users, NFTs, closed auction and
+    amount of money collected.Data will update every 24 hours"""
+
+    @method_decorator(cache_page(86400))
+    def get(self, request, *args, **kwargs):
+        users = User.objects.count()
+        auctions = Auction.objects.filter(is_open=False)
+        collected = 0
+        web3 = Web3(Web3.HTTPProvider("http://network:8545"))
+        for auction in auctions:
+            collected += web3.from_wei(
+                web3.eth.get_balance(auction.wallet),
+                "ether"
+            )
+        return Response(
+            {
+                "users": users,
+                "gifted_nfts": 0,
+                "closed_auctions": auctions.count(),
+                "money_collected": collected
+            }
+        )
